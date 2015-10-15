305b7d3c8b7bb3108803973f0abc6e4c5f0302fd
AustinScott3573 <Ascott3573@gmail.com>
2015-10-15 08:54:00 -0500

    Second Commit. Added Birthday.

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -10,23 +10,63 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello, World");
+
+            // Step 1: Getting user's name and print it.
             System.Console.WriteLine("What is your name?");
             string name = System.Console.ReadLine();
             System.Console.WriteLine("Welcome, " + name);
-            string upperName = name.ToUpper();
 
-            foreach (char letter in name.ToLower())
+            // Step 2: Cheer the user on.
+            // If name is "Bob", output should be:
+            // Give me a.. b
+            // Give me an.. o
+            // Give me a.. b
+            // BOB is.. GRAND!
+            string lower = name.ToLower();
+            string vowels = "aeioumnxsfhlr";
+
+            foreach (char letter in lower)
             {
-                string article = "a";
-                if ("halfnorsemix".IndexOf(letter) >= 0)
+                if (vowels.IndexOf(letter) != -1)
                 {
-                    article = "an";
+                    Console.WriteLine("Give me an.. " + letter);
                 }
-                System.Console.WriteLine("Give me " + article + ". . " + letter);
+                else
+                {
+                    Console.WriteLine("Give me a.. " + letter);
+                }
             }
+            string upper = name.ToUpper();
+            Console.WriteLine(upper + " is.. semi-cool, I guess.");
 
-            System.Console.WriteLine(upperName + " is.. cool, I guess");
-
+            // Asks user for birthdate
+            System.Console.WriteLine("When is your birthday? Use MM/DD");
+            string birthdate = System.Console.ReadLine();
+            DateTime today = DateTime.Now;
+            string year = today.Year.ToString();
+            var bd = DateTime.Parse(birthdate + "/" + year);
+            TimeSpan timeDiff = bd - today;
+            var daysTil = Convert.ToInt32(timeDiff.TotalDays);
+            System.Console.WriteLine(daysTil);
+            if (daysTil == 0)
+            {
+                System.Console.WriteLine("Happy Birthday!");
+            }
+            if (daysTil < 0)
+            {
+                daysTil = 365 - daysTil;
+            }
+            if (daysTil > 300)
+            {
+                System.Console.WriteLine("Sorry we missed it :/");
+            }
+            else
+            {
+                System.Console.WriteLine("Your birthday is in {0} days!!", daysTil);
+            }
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
         }
     }
 }
